12dfa52ed41b3ae3ad8beb7ad60d576e819237eb
James <james@trbl.design>
2022-11-19 11:50:00 -0500

    chore: removes old files

--- api/[collection]/init.js
+++
@@ -1,8 +0,0 @@
-import initOperation from 'payload/dist/auth/operations/init'
-import payload from '../../dist/payload'
-
-export default async function handler(req, res) {
-  const Model = payload.collections[req.query.collection].Model
-  const initialized = await initOperation({ req, Model })
-  return res.status(200).json({ initialized })
-}
