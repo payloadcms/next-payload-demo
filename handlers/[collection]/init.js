cc4bca37ee597e09b0b9db2134f171b1134f2feb
James <james@trbl.design>
2022-11-20 10:28:23 -0500

    chore: returns proper initialized value

--- handlers/[collection]/init.js
+++ handlers/[collection]/init.js
@@ -5,5 +5,5 @@ module.exports = async function handler(req, res) {
   const payload = getPayload()
   const Model = payload.collections[req.query.collection].Model
   const initialized = await initOperation({ req, Model })
-  return res.status(200).json({ initialized: true })
+  return res.status(200).json({ initialized })
 }
