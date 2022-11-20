56851aba3e9ed79718d5989151bda30993ee6a81
James <james@trbl.design>
2022-11-20 10:59:07 -0500

    chore: reads config file in a better spot

--- payload.js
+++ payload.js
@@ -1,9 +1,15 @@
 const payload = require('payload')
+const path = require('path')
+const fs = require('fs')
 
 let initializedPayload = null;
 
 module.exports = function getPayload() {
   if (!initializedPayload) {
+    const file = path.resolve(process.cwd(), './dist/payload.config.js');
+    // Need to read config file to force Vercel to include it in output
+    fs.readFileSync(file, 'utf8');
+    
     payload.init({
       local: true,
       mongoURL: process.env.MONGODB_URI,
