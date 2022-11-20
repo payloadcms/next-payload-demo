336198e0e4f3a1c32a5c6a424271cb62bf195363
James <james@trbl.design>
2022-11-20 08:40:08 -0500

    chore: further console logging

--- payload.js
+++ payload.js
@@ -1,10 +1,18 @@
 const payload = require('payload')
+const fs = require('fs')
 
 const initializedPayload = null;
 
 module.exports = function getPayload() {
   console.log({ cwd: process.cwd() })
   console.log({ config: process.env.PAYLOAD_CONFIG_PATH })
+
+  fs.readdir(process.cwd(), (err, files) => {
+    files.forEach(file => {
+      console.log(file);
+    });
+  });
+  
   if (!initializedPayload) {
     payload.init({
       local: true,
