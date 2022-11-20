04b452f805fe042d9c4c11df83ab027d6bc9c5c0
James <james@trbl.design>
2022-11-20 10:14:25 -0500

    chore: restores payload init

--- handlers/[collection]/init.js
+++ handlers/[collection]/init.js
@@ -3,7 +3,7 @@ const getPayload = require('../../payload')
 
 module.exports = async function handler(req, res) {
   const payload = getPayload()
-  // const Model = payload.collections[req.query.collection].Model
-  // const initialized = await initOperation({ req, Model })
+  const Model = payload.collections[req.query.collection].Model
+  const initialized = await initOperation({ req, Model })
   return res.status(200).json({ initialized: true })
 }
--- payload.js
+++ payload.js
@@ -1,26 +1,16 @@
 const payload = require('payload')
-const fs = require('fs')
 
 let initializedPayload = null;
 
 module.exports = function getPayload() {
-  console.log({ cwd: process.cwd() })
-  console.log({ config: process.env.PAYLOAD_CONFIG_PATH })
-
-  fs.readdir(process.cwd(), (err, files) => {
-    files.forEach(file => {
-      console.log(file);
-    });
-  });
-
   if (!initializedPayload) {
-    // payload.init({
-    //   local: true,
-    //   mongoURL: process.env.MONGODB_URI,
-    //   secret: process.env.PAYLOAD_SECRET,
-    // });
+    payload.init({
+      local: true,
+      mongoURL: process.env.MONGODB_URI,
+      secret: process.env.PAYLOAD_SECRET,
+    });
   
-    // initializedPayload = payload;
+    initializedPayload = payload;
   }
 
   return initializedPayload
