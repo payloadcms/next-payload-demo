bca7891dd2fd1ad82159843dadb807d5be33484e
James <james@trbl.design>
2022-11-20 08:34:00 -0500

    chore: logging to debug

--- payload.js
+++ payload.js
@@ -3,6 +3,8 @@ const payload = require('payload')
 const initializedPayload = null;
 
 module.exports = function getPayload() {
+  console.log({ cwd: process.cwd() })
+  console.log({ config: process.env.PAYLOAD_CONFIG_PATH })
   if (!initializedPayload) {
     payload.init({
       local: true,
