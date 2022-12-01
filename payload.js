11a44afdf6e2bc00df3cb0864403486f56d7ddae
James <james@trbl.design>
2022-12-01 10:17:45 -0500

    chore: re-adds config fix

--- payload.js
+++ payload.js
@@ -1,14 +1,14 @@
 const payload = require('payload')
-// const path = require('path')
-// const fs = require('fs')
+const path = require('path')
+const fs = require('fs')
 
 let initializedPayload = null;
 
 module.exports = function getPayload() {
   if (!initializedPayload) {
-    // const file = path.resolve(process.cwd(), './dist/payload.config.js');
-    // // Need to read config file to force Vercel to include it in output
-    // fs.readFileSync(file, 'utf8');
+    const file = path.resolve(process.cwd(), './dist/payload.config.js');
+    // Need to read config file to force Vercel to include it in output
+    fs.readFileSync(file, 'utf8');
     
     payload.init({
       local: true,
