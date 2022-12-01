8c027b97f61574c0b0266c5f4a7ad1b36d9779d6
James <james@trbl.design>
2022-12-01 10:25:56 -0500

    chore: removes serverURL

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,17 +1,13 @@
 import { buildConfig } from 'payload/config';
 import path from 'path';
-// import Examples from './collections/Examples';
 import Users from './collections/Users';
 
 export default buildConfig({
-  serverURL: 'http://localhost:3000',
   admin: {
     user: Users.slug,
   },
   collections: [
     Users,
-    // Add Collections here
-    // Examples,
   ],
   typescript: {
     outputFile: path.resolve(__dirname, 'payload-types.ts'),
