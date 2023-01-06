f9d1a393374d74ffc0af94e098b882c474578a14
Jacob Fletcher <jacobsfletch@gmail.com>
2023-01-06 10:03:18 -0500

    poc: payload init

--- payload.js
+++ payload.js
@@ -2,40 +2,22 @@ const payload = require('payload')
 const path = require('path')
 const fs = require('fs')
 
-/**
- * Global is used here to maintain a cached connection across hot reloads
- * in development. This prevents connections growing exponentially
- * during API Route usage.
- */
-let cached = global.payload
+let initializedPayload = null;
 
-if (!cached) {
-  cached = global.payload = { payload: null, promise: null }
-}
-
-module.exports = async function getPayload() {
-  const file = path.resolve(process.cwd(), './dist/payload.config.js');
-  // Need to read config file to force Vercel to include it in output
-  fs.readFileSync(file, 'utf8');
-  
-  if (cached.payload) {
-    return cached.payload
-  }
-
-  if (!cached.promise) {
-    cached.promise = payload.initAsync({
+module.exports = function getPayload() {
+  if (!initializedPayload) {
+    const file = path.resolve(process.cwd(), './dist/payload.config.js');
+    // Need to read config file to force Vercel to include it in output
+    fs.readFileSync(file, 'utf8');
+    
+    payload.init({
       local: true,
       mongoURL: process.env.MONGODB_URI,
       secret: process.env.PAYLOAD_SECRET,
-    }).then(() => payload)
+    });
+  
+    initializedPayload = payload;
   }
 
-  try {
-    cached.payload = await cached.promise
-  } catch (e) {
-    cached.promise = null
-    throw e
-  }
-
-  return cached.payload
-}
+  return initializedPayload
+};
