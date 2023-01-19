623a10dca21ff4b9562e7fcefcde1242ee78a687
James <james@trbl.design>
2023-01-19 11:24:05 -0500

    chore: attempts to load dist config

--- getPayload.ts
+++ getPayload.ts
@@ -1,12 +1,14 @@
+import { SanitizedConfig } from 'payload/config'
 import { getPayload as getPayloadLocal } from 'payload/dist/payload'
-import { clientFiles } from 'payload/dist/config/clientFiles';
 
-clientFiles.forEach((ext) => {
-  require.extensions[ext] = () => null;
-});
+let config: SanitizedConfig
 
-// Need to statically import config to get Next to pick up on it
-import config from './payload/payload.config'
+if (process.env.NODE_ENV === 'production') {
+  // Need to statically import config to get Next to pick up on it
+  config = require('./dist/payload.config')
+} else {
+  config = require('./payload/payload.config')
+}
 
 const getPayload = async () => {
   return getPayloadLocal({
