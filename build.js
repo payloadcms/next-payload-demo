b00277c343167d54d98fb145e7371e3133b92e07
Jarrod Flesch <jarrodmflesch@gmail.com>
2023-01-20 11:41:23 -0500

    chore: adjusts file structure so relative paths to /middleware are the same in handlers and pages

--- build.js
+++ build.js
@@ -5,7 +5,7 @@ const args = process.argv.slice(2); // nodejs command line args are an array tha
 const apiOutputPath = args[0] || './api'
 
 // Copy handlers into /api
-copyRecursiveSync(path.resolve(__dirname, './handlers'), path.resolve(__dirname, apiOutputPath))
+copyRecursiveSync(path.resolve(__dirname, './handlers/api'), path.resolve(__dirname, apiOutputPath))
 
 // Copy built admin JS into /public/admin
 copyRecursiveSync(path.resolve(__dirname, './build'), path.resolve(__dirname, './public/admin'))
--- pages/api/[collection]/me.ts
+++ pages/api/[collection]/me.ts
@@ -3,13 +3,10 @@ import withPayload from '../../../middleware/withPayload'
 import authenticate from '../../../middleware/authenticate'
 import initializePassport from '../../../middleware/initializePassport'
 import withDataLoader from '../../../middleware/dataLoader'
-import { timestamp } from '../../../utilities/timestamp'
 
 async function handler(req, res) {
   const collection = req.payload.collections[req.query.collection]
-  timestamp('before me')
   const result = await meOperation({ req, collection })
-  timestamp('after me')
   return res.status(200).json(result)
 }
 
