c13c2a16c6bc9a8102743285d0d5f5770e4ad1d6
James <james@trbl.design>
2023-01-19 21:47:03 -0500

    chore: adds timestamps

--- middleware/withPayload.ts
+++ middleware/withPayload.ts
@@ -1,7 +1,10 @@
+import { timestamp } from '../utilities/timestamp'
 import getPayload from '../getPayload'
 
 const withPayload = (handler) => async (req, res) => {
+  timestamp('before')
   req.payload = await getPayload()
+  timestamp('after')
   handler(req, res)
 }
 
