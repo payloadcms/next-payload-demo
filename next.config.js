db3601487910f15cebc00467b0820ecb6bdaf46b
James <james@trbl.design>
2023-03-25 13:36:15 -0400

    chore: excludes further dependencies

--- next.config.js
+++ next.config.js
@@ -10,8 +10,10 @@ const nextConfig = withPayload(
         "**/*": [
           "node_modules/@swc/core-linux-x64-gnu",
           "node_modules/@swc/core-linux-x64-musl",
+          "node_modules/@swc/wasm",
           "node_modules/sharp",
-          "node_modules/webpack",
+          "node_modules/webpack/lib",
+          "node_modules/webpack/schemas",
         ],
       },
     },
