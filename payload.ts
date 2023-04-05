8f6bc5ce3e903acfe6d6709797a399846b98267a
James <james@trbl.design>
2023-04-05 14:04:58 -0400

    chore: adds timestamps for debugging

--- payload.ts
+++ payload.ts
@@ -1,14 +1,21 @@
 import { getPayload } from "payload/dist/payload";
 import config from "./payload/payload.config";
+import { timestamp } from "./utilities/timestamp";
 
 const getInitializedPayload = async () => {
-  return getPayload({
+  timestamp('before getting payload')
+
+  const payload = await getPayload({
     // Make sure that your environment variables are filled out accordingly
     mongoURL: process.env.MONGODB_URI as string,
     secret: process.env.PAYLOAD_SECRET as string,
     // Notice that we're passing our Payload config
     config,
   });
+
+  timestamp('after getting payload')
+
+  return payload
 };
 
 export default getInitializedPayload;
