4b53d84d549ce6fc1d12ce7d544d45e1bbf30b6b
Jacob Fletcher <jacobsfletch@gmail.com>
2023-07-11 09:48:40 -0400

    poc: adds UITest back

--- payload/components/UITest/index.tsx
+++ payload/components/UITest/index.tsx
@@ -1,13 +1,13 @@
 'use client'
 
 import React from 'react';
-// import { Label } from 'payload/components/forms'
+import { Label } from 'payload/components/forms'
 import './index.scss'
 
 export const UIField: React.FC = () => {
   return (
     <React.Fragment>
-      {/* <Label label="Hello from a custom UI component" required /> */}
+      <Label label="Hello from a custom UI component" required />
       <p className="test">hello from a custom UI component</p>
     </React.Fragment>
   )
